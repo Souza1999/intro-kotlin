ebff3320c6e03b6387ad5ba586744a321668f947
Yuri Souza <bryurisouza@outlook.com.br>
2024-03-12 09:58:17 -0300

    Atualizado a prática da sala de aula

--- src/main/kotlin/Aula2-OO.kt
+++ src/main/kotlin/Aula2-OO.kt
@@ -1,30 +1,25 @@
-Spackage br.ifpb.pdm
+package br.ifpb.pdm
 
 fun main() {
     val repositorioAnimal = RepositorioAnimal()
     var opcao = 0
-    while (opcao != 6) {
+    while (opcao != 7) {
         menu()
         print("Digite a opção: ")
-        opcao = readlnOrNull()?.toInt() ?: 0
+        opcao = readlnOrNull()?.toInt() ?: 9
         when (opcao) {
-            0 -> {
-                val animal = Animal(10)
-                animal.nome = "genérico"
-                repositorioAnimal.adicionar(animal)
-            }
             1 -> {
-                val cachorro = Cachorro(10)
+                val cachorro = Cachorro(10, Color.preto)
                 cachorro.nome = "Rex"
                 repositorioAnimal.adicionar(cachorro)
             }
             2 -> {
-                val gato = Gato(5)
+                val gato = Gato(5, Color.vermelho)
                 gato.nome = "Felix"
                 repositorioAnimal.adicionar(gato)
             }
             3 -> {
-                val passaro = Passaro(2)
+                val passaro = Passaro(2, Color.amarelo)
                 passaro.nome = "Piu"
                 repositorioAnimal.adicionar(passaro)
             }
@@ -32,15 +27,19 @@ fun main() {
                 repositorioAnimal.listar()
             }
             5 -> {
-                repositorioAnimal.animais.forEach(Animal::emitirSom)
+                //repositorioAnimal.animais.forEach(Animal::emitirSom)
                 repositorioAnimal.animais.forEach { it.emitirSom()}
+            }
+            6 -> {
+                //repositorioAnimal.animais.forEach(Animal::exibirCor)
+                repositorioAnimal.animais.forEach { it.exibirCor()}
             }
         }
 
     }
 }
 
-open class Animal(var idade: Int) {
+abstract class Animal(var idade: Int, var cor: Color) {
     open var nome: String = ""
         get() = "Animal: $field"
         set(valor) {
@@ -48,11 +47,24 @@ open class Animal(var idade: Int) {
         }
 
     open fun emitirSom() {
-        println("Som de animal")
+
+    }
+
+    open fun exibirCor() {
+
+    }
+
+    open fun idadeHumanos() {
+
     }
 }
 
-class Cachorro(idade: Int) : Animal(idade) {
+enum class Color() {
+    amarelo, preto, vermelho
+}
+
+
+class Cachorro(idade: Int, cor: Color) : Animal(idade, cor) {
     override var nome: String = ""
         get() = field
         set(valor) {
@@ -61,27 +73,54 @@ class Cachorro(idade: Int) : Animal(idade) {
     override fun emitirSom() {
         println("Au au")
     }
+
+    override fun exibirCor() {
+        println("Cor do cachorro: $cor")
+    }
+
 }
-class Gato(idade: Int) : Animal(idade) {
+
+
+class Gato(idade: Int, cor: Color) : Animal(idade, cor) {
+    override var nome: String = ""
+        get() = field
+        set(valor) {
+            field = valor
+        }
     override fun emitirSom() {
         println("Miau")
     }
+
+    override fun exibirCor() {
+        println("Cor do gato: $cor")
+    }
+
 }
 
-class Passaro(idade: Int) : Animal(idade) {
+
+class Passaro(idade: Int, cor: Color) : Animal(idade, cor) {
+    override var nome: String = ""
+        get() = field
+        set(valor) {
+            field = valor
+        }
     override fun emitirSom() {
         println("Piu piu")
+    }
+
+    override fun exibirCor() {
+        println("Cor do passaro: $cor")
     }
 }
 
 fun menu() {
-    println("0 - Animal")
     println("1 - Cachorro")
     println("2 - Gato")
     println("3 - Pássaro")
     println("4 - Listar Animais")
     println("5 - Emitir som")
-    println("6 - Sair")
+    println("6 - Exibir cor")
+    println("7 - Sair")
 }
 
 class RepositorioAnimal {
@@ -95,4 +134,3 @@ class RepositorioAnimal {
         animais.forEach { println(it.nome) }
     }
 }
-
